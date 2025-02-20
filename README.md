56525866526d7b610f97db6185334869ac00853e
Alexander MacKinnon <57075308+alexandermackinnon@users.noreply.github.com>
2025-02-19 23:45:29 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# postcards-from-my-jungle
+# Postcards from my Jungle
+
+## Model Training
+
+To be written.
+
+## Image and Sound Generation
+
+To be written.
+
+## Insights Gained and Possible Improvements
+
+To be written.
---
+++ inc/script.js
@@ -0,0 +1,7 @@
+// ========== Wait for DOM to Load ==========
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("Document fully loaded and parsed!");
+
+  // Call functions here if needed
+  initNavigation();
+});
